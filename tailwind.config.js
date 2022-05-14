b06d2aafd241aa6aa8cebc47465ce0b41e0a7c17
angelxv01 <angeloxv@outlook.it>
2022-05-14 11:46:24 +0200

    reposition on first two sections

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    colors: {
+      transparent: "transparent",
+      white: "#fff",
+      black: "#000",
+      "soft-blue": "#5368DF",
+      "soft-red": "#FA5757",
+      "neutral-blue": "#9194A1",
+      "dark-blue": "#252B46",
+    },
+    fontFamily: {
+      sans: "Rubik",
+    },
+  },
+};
